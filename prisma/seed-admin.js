1fe7ff2b28e44ebc46ae2edb6e62e5ef4c2093f2
Jhon <perezjhon2010@alumnos.cei.es>
2026-04-09 20:34:19 +0200

    ajustando scheema

--- prisma/seed-admin.js
+++ prisma/seed-admin.js
@@ -1,18 +1,11 @@
 const { PrismaClient } = require('@prisma/client')
 const bcrypt = require('bcryptjs')
 
-// Hardcode the connection for seeding
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: 'postgresql://postgres.fifaolmsgghwsaktdffe:4400453641861814@aws-1-eu-west-2.pooler.supabase.com:5432/postgres'
-    }
-  }
-})
+// Usa la variable de entorno 
+const prisma = new PrismaClient()
 
 async function main() {
   console.log('🌱 Creating admin user...')
-
   const adminPassword = await bcrypt.hash('admin123', 12)
   const admin = await prisma.user.create({
     data: {
